4469f1ab0b94b0163a60df877c07195e5a791059
AdmiralSlowpoke <maksimzlt@gmail.com>
2020-08-10 22:05:34 +0500

    Game
    Рамки экрана и мобильное управление

---
+++ Assets/BoundCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundCheck : MonoBehaviour
+{
+    public float radius = 1f;
+    private float _camWidth;
+    private float _camHeight;
+    private void Awake()
+    {
+        _camHeight = Camera.main.orthographicSize;
+        _camWidth = _camHeight * Camera.main.aspect;
+    }
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+        if (pos.x > _camWidth - radius)
+            pos.x = _camWidth - radius;
+        if (pos.x < -_camWidth + radius)
+            pos.x = -_camWidth + radius;
+        if (pos.y > _camHeight - radius)
+            pos.y = _camHeight - radius;
+        if (pos.y < -_camHeight + radius)
+            pos.y = -_camHeight + radius;
+
+        transform.position = pos;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed=15f;
+    public DynamicJoystick joystick;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 gamepadInput = new Vector3(joystick.Horizontal,joystick.Vertical,0);
+        transform.position += (gamepadInput + new Vector3(x, y, 0))*speed*Time.deltaTime;
+    }
+}
