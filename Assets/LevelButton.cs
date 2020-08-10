27267cbad32eb2c732eb5c4f39659eee1d53b89e
AdmiralSlowpoke <maksimzlt@gmail.com>
2020-08-10 20:24:18 +0500

    Level Selection
    Выбор уровня

---
+++ Assets/LevelButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour
+{
+    private Text _buttonText;
+    public enum State {Complete,Open,Locked};//Статус кнопки
+    private void Start()
+    {
+        _buttonText = gameObject.GetComponentInChildren<Text>();
+    }
+    public void SetLevelText(int level)
+    {
+        _buttonText.text = level.ToString(); //Сеттер номера уровня
+    }
+    public void SetButtonStatus(State status)//Установка статуса кнопки
+    {
+        switch (status)
+        {
+            case State.Complete:
+                Button b = gameObject.GetComponent<Button>();
+                ColorBlock cb = b.colors;
+                cb.normalColor = Color.yellow;
+                b.colors = cb;
+                break;
+            case State.Open:
+                gameObject.GetComponent<Button>().interactable = true;
+                break;
+            case State.Locked:
+                gameObject.GetComponent<Button>().interactable = false;
+                break;
+
+        }
+    }
+    public void clickOnButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
